27bb9856469911fc2b8ec8d5a3348fa1217f67c0
ShortArrow <bamboogeneral@live.jp>
2019-03-22 03:31:29 +0900

    add table

--- routes/mytable.js
+++ routes/mytable.js
@@ -1,12 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
+var mysql = require('mysql');
+
+//mysql of config
+var mysql_setting = {
+    host: 'localhost',
+    user: 'root',
+    password: 'Robotics',
+    database: 'my-nodeapp-db'
+};
+
+// get access of prosec]s\\
 router.get('/', (req, res, next) => {
-    var data = {
-        title: 'MyTable!',
-        content: 'これは、サンプルのコンテンツです。<br>this is sample contents.'
-    };
-    res.render('Mytable', data);
+    // ready connection
+    var connection = mysql.createConnection(mysql_setting);
+    // connect base
+    connection.connect();
+
+    // read data
+    connection.query('SELECT * from mydata', function (error, results, fields) {
+        if (error == null) {
+            var data = {
+                title: 'mysql',
+                content: results
+            };
+            res.render('hello', data);
+        }
+    });
+
+    connection.end();
 });
 
 module.exports = router;
