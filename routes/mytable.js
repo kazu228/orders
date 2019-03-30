b43ecc62aadbd9188788cd12ba567cf95c603f47
ShortArrow <bamboogeneral@live.jp>
2019-03-30 11:19:33 +0900

    add create page

--- routes/mytable.js
+++ routes/mytable.js
@@ -11,7 +11,7 @@ var mysql_setting = {
     database: 'my-nodeapp-db'
 };
 
-// get access of prosec]s\\
+// get access of process\\
 router.get('/', (req, res, next) => {
     // ready connection
     var connection = mysql.createConnection(mysql_setting);
@@ -25,10 +25,37 @@ router.get('/', (req, res, next) => {
                 title: 'mysql',
                 content: results
             };
-            res.render('mytable', data);
+            res.render('mytable/index', data);
         }
     });
 
+    connection.end();
+});
+
+// get access of add
+router.get('/add', (req, res, next) => {
+    var data = {
+        title: 'Mytable/Add',
+        content: '新しいレコードを入力：'
+    }
+    res.render('mytable/add', data);
+});
+
+// post of add
+router.post('/add', (req, res, next) => {
+    var nm = req.body.name;
+    var ml = req.body.mail;
+    var ag = req.body.age;
+    var data = {
+        'name': nm,
+        'mail': ml,
+        'age': ag
+    };
+    var connection = mysql.createConnection(mysql_setting);
+    connection.connect();
+    connection.query('insert into mydata set ?', data, function (error, results, fields) {
+        res.redirect('/mytable');
+    });
     connection.end();
 });
 
