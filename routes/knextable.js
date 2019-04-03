5f38cf8ed5d3f7071182d914ea1cff67b2f75ea1
ShortArrow <bamboogeneral@live.jp>
2019-04-03 16:39:07 +0900

    knex add findpage

--- routes/knextable.js
+++ routes/knextable.js
@@ -3,6 +3,23 @@ var router = express.Router();
 
 var mysql = require('mysql');
 
+//knex
+var knex = require('knex')({
+    dialect: 'mysql',
+    connection: {
+        host: '192.168.0.220',
+        user: 'root',
+        password: 'kikaku',
+        database: 'my-nodeapp-db',
+        charset: 'utf8'
+    }
+});
+
+var Bookshelf = require('bookshelf')(knex);
+var MyData = Bookshelf.Model.extend({
+    tableName: 'mydata'
+});
+
 //mysql of config
 var mysql_setting = {
     host: '192.168.0.220',
@@ -13,23 +30,20 @@ var mysql_setting = {
 
 // get access of process\\
 router.get('/', (req, res, next) => {
-    // ready connection
-    var connection = mysql.createConnection(mysql_setting);
-    // connect base
-    connection.connect();
-
-    // read data
-    connection.query('SELECT * from mydata', function (error, results, fields) {
-        if (error == null) {
-            var data = {
-                title: 'mysql',
-                content: results
-            };
-            res.render('knextable/index', data);
-        }
+    new MyData().fetchAll().then((collection) => {
+        var data = {
+            title: 'Hello!',
+            content: collection.toArray()
+        };
+        res.render('knextable/index', data);
+    }).catch((err) => {
+        res.status(500).json({
+            error: true,
+            data: {
+                message: err.message
+            }
+        });
     });
-
-    connection.end();
 });
 
 // get access of add
@@ -37,26 +51,16 @@ router.get('/add', (req, res, next) => {
     var data = {
         title: 'knextable/Add',
         content: '新しいレコードを入力：'
-    }
+    };
     res.render('knextable/add', data);
 });
 
 // post of add
 router.post('/add', (req, res, next) => {
-    var nm = req.body.name;
-    var ml = req.body.mail;
-    var ag = req.body.age;
-    var data = {
-        'name': nm,
-        'mail': ml,
-        'age': ag
-    };
-    var connection = mysql.createConnection(mysql_setting);
-    connection.connect();
-    connection.query('insert into mydata set ?', data, (error, results, fields) => {
-        res.redirect('/knextable');
+    var response = res;
+    new MyData(req.body).save().then((model) => {
+        response.redirect('/knextable');
     });
-    connection.end();
 });
 
 // get of add
@@ -143,4 +147,58 @@ router.post('/delete', (req, res, next) => {
     connection.end();
 });
 
+router.get('/find', (req, res, next) => {
+    var data = {
+        title: '/knex/find',
+        content: '検索IDを入力：',
+        form: {
+            fstr: ''
+        },
+        mydata: null
+    };
+    res.render('knextable/find', data);
+});
+
+router.post('/find', (req, res, next) => {
+    new MyData().where('id', '=', req.body.fstr).fetch().then((collection) => {
+        var data = {
+            title: 'knextable!',
+            content: '※ID = ' + req.body.fstr + 'の検索結果：',
+            form: req.body,
+            mydata: collection
+        };
+        res.render('knextable/find', data);
+        // res.render('knextable/find');
+    });
+});
+
 module.exports = router;
+
+Bookshelf.plugin('pagination');
+
+router.get('/:page', (req, res, next) => {
+    var pg = req.params.page;
+    pg *= 1;
+    if (pg < 1) {
+        pg = 1;
+    }
+    new MyData().fetchPage({
+        page: pg,
+        pageSize: 3
+    }).then((collection) => {
+        var data = {
+            title: 'Jellop1!',
+            content: collection.toArray(),
+            pagenation: collection.pagenation
+        };
+        // console.log(collection.pagenation);
+        res.render('knextable/index', data);
+    }).catch((err) => {
+        res.status(500).json({
+            error: true,
+            data: {
+                message: err.message
+            }
+        });
+    });
+});
